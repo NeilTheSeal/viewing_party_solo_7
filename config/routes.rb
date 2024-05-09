225908cab3f711763a6e6ba4ee9ad6ed65da1032
NeilTheSeal <neil.hendren@gmail.com>
2024-05-09 12:54:04 -0600

    Implement user story 1

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
 
-
   root "welcome#index"
-  get '/register', to: 'users#new', as: 'register_user'
+  get "/register", to: "users#new", as: "register_user"
 
-  resources :users, only: [:show, :create] do
+  resources :users, only: %i[show create] do
+    resources :discover, only: %i[index]
   end
 end
