34e8c9905c7c703c5bdac83ca2aa75c977128c69
NeilTheSeal <neil.hendren@gmail.com>
2024-05-10 12:07:48 -0600

    Update to have dynamic cassettes

--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -8,38 +8,63 @@ RSpec.describe "Discover Movies" do
     )
   end
 
-  it "can list the most popular movies", :vcr do
-    # VCR.use_cassette("name_of_cassette") do
+  it "can list the most popular movies" do
+    VCR.use_cassette("popular_movies", serialize_with: :json) do |cassette|
+      visit "/users/#{@user.id}/discover"
 
-    # end
-    visit "/users/#{@user.id}/discover"
+      click_link "Top Rated Movies"
 
-    click_link "Top Rated Movies"
+      expect(page).to have_current_path(
+        "/users/#{@user.id}/movies",
+        ignore_query: true
+      )
+      expect(page.status_code).to eq 200
 
-    expect(page).to have_current_path(
-      "/users/#{@user.id}/movies",
-      ignore_query: true
-    )
-    expect(page.status_code).to eq 200
-    within "#movies" do
-      expect(page).to have_css(".movie-info", count: 20)
-      expect(page).to have_css("a", count: 20)
+      body = JSON.parse(
+        cassette.serializable_hash.dig(
+          "http_interactions", 0, "response", "body", "string"
+        ),
+        symbolize_names: true
+      )
+
+      movies = body[:results]
+
+      within "#movies" do
+        expect(page).to have_css(".movie-info", count: 20)
+        expect(page).to have_css("a", count: 20)
+        movies.each do |movie|
+          expect(page).to have_content(movie[:title])
+        end
+      end
     end
   end
 
   it "can find a movie by title search", :vcr do
-    visit "/users/#{@user.id}/discover"
+    VCR.use_cassette("movie_search", serialize_with: :json) do |cassette|
+      visit "/users/#{@user.id}/discover"
 
-    fill_in :search, with: "Super Troopers"
-    click_button "Search"
+      fill_in :search, with: "Super Troopers"
+      click_button "Search"
 
-    expect(page).to have_current_path(
-      "/users/#{@user.id}/movies",
-      ignore_query: true
-    )
-    expect(page.status_code).to eq 200
-    expect(page).to have_content("Super Troopers")
-    expect(page).to have_content("Super Troopers 2")
-    expect(page).to have_content("Super Troopers 3: Winter Soldiers")
+      expect(page).to have_current_path(
+        "/users/#{@user.id}/movies",
+        ignore_query: true
+      )
+
+      expect(page.status_code).to eq 200
+
+      body = JSON.parse(
+        cassette.serializable_hash.dig(
+          "http_interactions", 0, "response", "body", "string"
+        ),
+        symbolize_names: true
+      )
+
+      movies = body[:results]
+
+      expect(page).to have_content(movies[0][:title])
+      expect(page).to have_content(movies[1][:title])
+      expect(page).to have_content(movies[2][:title])
+    end
   end
 end
